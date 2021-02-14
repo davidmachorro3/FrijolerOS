ddf0494feff4a1afadf1787021fdfb1831491b89
Oswaldo Lopez <oswaldo.aquino@galileo.edu>
2021-02-14 14:50:36 -0800

    lock acquire done

--- threads/synch.c
+++ threads/synch.c
@@ -196,6 +196,12 @@ lock_acquire (struct lock *lock)
   ASSERT (!intr_context ());
   ASSERT (!lock_held_by_current_thread (lock));
 
+  if(lock-> holder != NULL && thread_current()-> priority > (lock-> holder) -> priority)
+  {
+    old_priority = (lock-> holder) -> priority;
+    (lock-> holder) ->priority = thread_current()->priority;
+  }
+
   sema_down (&lock->semaphore);
   lock->holder = thread_current ();
 }
