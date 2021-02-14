24c7fc2adf867b5a8442f9e9b7ecced4e54e61b3
Oswaldo Lopez <oswaldo.aquino@galileo.edu>
2021-02-14 15:02:10 -0800

    defini old_priority (bug)

--- threads/synch.c
+++ threads/synch.c
@@ -189,8 +189,9 @@ lock_init (struct lock *lock)
    interrupt handler.  This function may be called with
    interrupts disabled, but interrupts will be turned back on if
    we need to sleep. */
-void
-lock_acquire (struct lock *lock)
+
+int old_priority = 0;
+void lock_acquire (struct lock *lock)
 {
   ASSERT (lock != NULL);
   ASSERT (!intr_context ());
